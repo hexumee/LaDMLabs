b1c99fa35433f811dafacb20b6349bc346e153bd
Dmitriy Nuzhdov <hexumee@gmail.com>
2022-03-09 21:21:42 +0400

    Исправления в л/р №2 и №3

--- lab3_func/scripts/mod.js
+++ lab3_func/scripts/mod.js
@@ -2,8 +2,9 @@
 // Определение переменных //
 ////////////////////////////
 let relMatrix = [];
-let elements = [];
-let e_set = [];
+let elements_a = [];
+let elements_b = [];
+let elements_r = [];
 let restrictedSymbols = ["`", "~", "!", "@", '"', "#", "№", "$", ";", "%", "^", ":", "&", "?", "*", "(", ")", "-",
                          "_", "=", "+", "[", "]", "{", "}", "'", "<", ">", ",", ".", "/", "|", "\\"];
 
@@ -15,9 +16,9 @@ let restrictedSymbols = ["`", "~", "!", "@", '"', "#", "№", "$", ";", "%", "^"
 function formatView() {
     let s = "";
 
-    for (let i = 0; i < e_set.length; i++) {
-        for (let j = 0; j < e_set.length; j++) {
-            if (j !== e_set.length-1) {
+    for (let i = 0; i < elements_a.length; i++) {
+        for (let j = 0; j < elements_b.length; j++) {
+            if (j !== elements_b.length-1) {
                 s += relMatrix[i][j] + " ";
             } else {
                 s += relMatrix[i][j];
@@ -31,128 +32,70 @@ function formatView() {
 
 // построение матрицы отношения
 function buildRelationMatrix() {
-    relMatrix = [];
-    e_set = [];
+    initMatrix();
 
-    elements.forEach(v => {
-        e_set.push(v[0]);
-        e_set.push(v[1]);
+    elements_r.forEach(val => {
+        let pair = val.replace(/ +/g, ' ').trim().split(" ");
+        relMatrix[elements_a.indexOf(pair[0])][elements_b.indexOf(pair[1])] = 1;
     });
-
-    e_set = Array.from(new Set(e_set)).sort();    // а надо ли сортировать?
-
-    for (let i = 0; i < e_set.length; i++) {
-        let matrixRow = [];
-
-        for (let j = 0; j < e_set.length; j++) {
-            // для всех e в elements проверяем на совпадение всех значений из [_k[i], _v[j]], т.е. if (e == [_k[i], _v[j]])
-            if (elements.some(e => [e_set[i], e_set[j]].every((v, idx) => v === e[idx]))) {
-                matrixRow.push(1);
-            } else {
-                matrixRow.push(0);
-            }
-        }
-
-        relMatrix.push(matrixRow);
-    }
 
     document.getElementById("resultMatrix").innerHTML = formatView();
 }
 
-// приведение к виду {(a, b), (x, y)...}
-function getHRV() {
-    let v0 = "";
-
-    elements.forEach((e, idx) => {
-        let t0 = `(${e[0]}, ${e[1]})`;
-        if (idx === 0) {
-            v0 += t0;
-        } else {
-            v0 += `, ${t0}`;
-        }
-    });
-
-    return `{${v0}}`;
-}
-
-// удаление лишних пробелов
-function strip(v) {
-    let r = [];
-    let idx = 0;
-
-    if (v.length !== 0) {
-        while (idx-1 !== v.length) {
-            let s = "";
-            while (v[idx] !== " ") {
-                s += v[idx];
-                idx++;
-                if (idx === v.length) {
-                    break;
-                }
-            }
-            if (s !== "") {
-                r.push(s);
-            }
-            idx++;
-            if (idx === v.length) {
-                break;
-            }
-        }
-    }
-
-    return r;
-}
-
 // проверка правильности ввода
-function validate(v0) {
+function validate(v0, v1, v2) {
     let isAlreadyFailed = false;
 
     restrictedSymbols.some(s => {
-        if (v0.includes(s)) {
+        if (v0.includes(s) || v1.includes(s)) {
             alert(`Обнаружен недопустимый символ: ${s}`);
             isAlreadyFailed = true;
         }
     });
 
-    if (!isAlreadyFailed && v0.length > 2) {
-        alert(`Слишком много элементов: ${v0.length}`);
+    if (!isAlreadyFailed && (v0[0] === "" || v1[0] === "")) {
+        alert(`Длина множества не должна быть равна нулю`);
         isAlreadyFailed = true;
     }
 
-    if (!isAlreadyFailed && v0.length < 2) {
-        alert(`Слишком мало элементов: их должно быть ровно два`);
-        isAlreadyFailed = true;
+    if (!isAlreadyFailed) {
+        let f = false;
+        v2.forEach(v => {
+            if (v.replace(/ +/g, ' ').trim().split(" ").length !== 2) {
+                isAlreadyFailed = true;
+                f = true;
+            }
+        });
+        if (f) {
+            alert(`В отношении указаны не пары элементов`);
+        }
     }
 
     return isAlreadyFailed === false;
 }
 
-// добавление элемента в отношение
-function add() {
-    let v0 = strip(document.getElementById("rf").value);
-
-    if (validate(v0)) {
-        elements.push(v0);
-        document.getElementById("pre").innerHTML = getHRV();
-    }
-}
-
-// удаление всех элементов из отношения
-function clearAll() {
-    elements = [];
-    document.getElementById("pre").innerHTML = "{}";
-}
-
-// удаление последнего элемента из отношения
-function clearLast() {
-    if (elements.length > 0) {
-        elements.pop();
-        document.getElementById("pre").innerHTML = getHRV();
+// инициализация матрицы
+function initMatrix() {
+    relMatrix = [];
+    for (let i = 0; i < (elements_a.length > elements_b.length ? elements_a.length : elements_b.length); i++) {
+        let row = [];
+        for (let j = 0; j < (elements_a.length > elements_b.length ? elements_a.length : elements_b.length); j++) {
+            row.push(0);
+        }
+        relMatrix.push(row);
     }
 }
 
 // анализ отношения
 function analyze() {
+    elements_a = Array.from(new Set(document.getElementById("rf").value.replace(/ +/g, ' ').trim().split(" "))).sort();
+    elements_b = Array.from(new Set(document.getElementById("sf").value.replace(/ +/g, ' ').trim().split(" "))).sort();
+    elements_r = Array.from(new Set(document.getElementById("tf").value.replace(/ +/g, ' ').trim().split(";")));
+
+    if (!(validate(elements_a, elements_b, elements_r))) {
+        return;
+    }
+
     buildRelationMatrix();
 
     let functionalR = true;
@@ -172,9 +115,9 @@ function analyze() {
         if (!functionalR) break;
     }
 
-    for (let i = 0; i < relMatrix.length; i++) {
+    for (let i = 0; i < relMatrix[0].length; i++) {
         let c = 0;
-        for (let j = 0; j < relMatrix[i].length; j++) {
+        for (let j = 0; j < relMatrix.length; j++) {
             if (relMatrix[j][i] === 1) {
                 c += 1;
             }
@@ -189,12 +132,11 @@ function analyze() {
     document.getElementById("result").innerHTML = `Данное отношение ${functionalR ? "" : "не "}является функцией A к B<br>Данное отношение ${functionalC ? "" : "не "}является функцией B к A`;
 }
 
-
 ///////////////////////
 // Обработка событий //
 ///////////////////////
-document.getElementById("rf").addEventListener("keyup", function(event) {
+document.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        document.getElementById("add").click();
+        document.getElementById("commit").click();
     }
 });
